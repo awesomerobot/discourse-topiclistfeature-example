9c1cd5d984f58e025f245920649548a6e4c2d582
awesomerobot <kris.aubuchon@discourse.org>
2022-06-22 13:57:18 -0400

    remove unused

--- javascripts/discourse/components/featured-topics.js
+++ javascripts/discourse/components/featured-topics.js
@@ -1,5 +1,4 @@
 import Component from "@ember/component";
-import { inject as controller } from "@ember/controller";
 import discourseComputed, { observes } from "discourse-common/utils/decorators";
 import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
